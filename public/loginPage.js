4b49ae37e7f14a2e2915e9d91454575ce099b3a2
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2020-04-14 12:35:14 -0700

    Firsst commit

---
+++ public/loginPage.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const userForm = new UserForm();
+
+userForm.loginFormCallback = data => {
+    // console.log(data);
+    // alert(JSON.stringify(data));
+    // ApiConnector.login(data,  userForm.loginFormAction(data));
+    
+
+
+    // ApiConnector.login(data, (error = null) => {
+        
+    // });
+}
+
