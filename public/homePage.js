163bd16587b997e3b777903f344eb35b211f4cef
Lana <Shuvaeva.lana@gmail.com>
2020-04-14 20:29:46 -0700

    Added functions - Profile Widget & Rates Board

--- public/homePage.js
+++ public/homePage.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// _____________________________ Log Out _____________________________ 
+
 const logOutButton = new LogoutButton();
 
 logOutButton.action = () => {
@@ -8,3 +10,29 @@ logOutButton.action = () => {
         console.log(response);
     });
 }
+
+
+// _____________________________  Profile Widget _____________________________ 
+
+ApiConnector.current(current => {
+    current.success ? ProfileWidget.showProfile(current.data) : console.error("Error: something went wrong");
+    console.log(current);
+});
+
+
+// _____________________________  Rates Board _____________________________ 
+
+const newRatesBoard = new RatesBoard();
+
+function fillRatesBoard() {
+    ApiConnector.getStocks(response => {
+        if (response) {
+            newRatesBoard.clearTable();
+            newRatesBoard.fillTable(response.data);
+        }
+    });
+}
+
+setInterval(function(){fillRatesBoard();}, 60000);
+
+
